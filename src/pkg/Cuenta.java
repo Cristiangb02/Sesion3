5a0d90a06a3f922fcd03d89a0fdb3c89342be354
cristian <cristian@Cristian>
2024-10-08 10:39:22 +0200

    Refactorizado el código ingresar

--- src/pkg/Cuenta.java
+++ src/pkg/Cuenta.java
@@ -3,15 +3,22 @@ package pkg;
 public class Cuenta {
 	
 	Double saldo;
+	
+	public Cuenta(Double saldo) {
+		this.saldo=saldo;
+	}
 
-	public Integer getSaldo() {
-		// TODO Auto-generated method stub
-		return 500;
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
 	}
 
 	public void ingresar(int i) {
-		// TODO Auto-generated method stub
-		
+		this.saldo+=i;
 	}
 
 }
